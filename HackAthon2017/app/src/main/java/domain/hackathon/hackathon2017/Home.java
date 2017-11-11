b3d8ba8857aab1a45c1bdf1fddd00ab8b5259c5e
Nilesh Kesar <NKesar@fullsail.edu>
2017-11-11 12:49:40 -0500

    movved it json file

--- HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Home.java
+++ HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Home.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
+    private String urlbase = "http://api.petfinder.com/";
+    private String urlmethod = "pet.getRandom?";
+    private String urlkey = "key=58fe2e272bebddbc0f5e66901f239055";
+    private String urlarg = "&output=basic";
+    private String urlShelter = "http://api.petfinder.com/shelter.get?key=58fe2e272bebddbc0f5e66901f239055&id=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
