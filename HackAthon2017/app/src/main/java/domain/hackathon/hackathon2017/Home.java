e82e70212b23f568406400bd98799364921ccfa2
On Lam <olam@fullsail.edu>
2017-11-11 17:12:11 -0500

    hgfjfj

--- HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Home.java
+++ HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Home.java
@@ -1,6 +1,5 @@
 package domain.hackathon.hackathon2017;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
@@ -17,8 +16,7 @@ import android.support.design.widget.NavigationView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class Home extends AppCompatActivity {
     private String urlbase = "http://api.petfinder.com/"; //base url
     private String urlkey = "key=58fe2e272bebddbc0f5e66901f239055"; //key for api
     private String urlmethodrandom = "pet.getRandom?"; //used for getting a random pet
@@ -32,114 +30,4 @@ public class Home extends AppCompatActivity
 
     private String urlShelter = "http://api.petfinder.com/shelter.get?key=58fe2e272bebddbc0f5e66901f239055&id=";
 
-    public static int petNumber;
-
-    private ViewStub stubGrid;
-    private GridView gridView;
-    private GridViewAdapter gridViewAdapter;
-
-    private List<PetInfo> petList = new ArrayList<>();
-
-    private DrawerLayout draw;
-    private ActionBarDrawerToggle toggle;
-
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class Home extends AppCompatActivity {
-
->>>>>>> parent of d2a1875... Merge branch 'master' of https://github.com/Hector7500/Hackathon2017
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-<<<<<<< HEAD
-
-        stubGrid = (ViewStub) findViewById(R.id.stub_grid);
-        stubGrid.inflate();
-        gridView = (GridView) findViewById(R.id.mygridview);
-        gridView.setOnItemClickListener(onItemClick);
-
-        draw = (DrawerLayout) findViewById(R.id.activity_home);
-        toggle = new ActionBarDrawerToggle(this, draw, R.string.open, R.string.close);
-        draw.addDrawerListener(toggle);
-        toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        NavigationView navigation = (NavigationView) findViewById(R.id.nav_view);
-        navigation.setNavigationItemSelectedListener(this);
-    }
-
-    AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            petNumber = petList.get(position).getPetnumber();
-            //startActivity(new Intent(Home.this, Description.class));
-        }
-    };
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.nav_home:
-                return true;
-            case R.id.nav_Search:
-                return true;
-            case R.id.nav_favorite:
-                return true;
-            case R.id.nav_logout:
-                return true;
-        }
-        if(toggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed()
-    {
-        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.activity_home);
-        if(drawerLayout.isDrawerOpen((GravityCompat.START)))
-            drawerLayout.closeDrawer(GravityCompat.START);
-        else
-            super.onBackPressed();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item)
-    {
-        int id = item.getItemId();
-
-        if(id==R.id.nav_home)
-        {
-            DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.activity_home);
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else if(id==R.id.nav_Search)
-        {
-            startActivity(new Intent(Home.this,search.class));
-        }
-        else if(id == R.id.nav_favorite)
-        {
-            //startActivity(new Intent(Home.this,Create_Event.class));
-        }
-
-        else if(id== R.id.nav_logout)
-        {
-            //startActivity(new Intent(Home.this,Profile.class));
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_home);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-=======
-    }
->>>>>>> parent of d2a1875... Merge branch 'master' of https://github.com/Hector7500/Hackathon2017
 }
